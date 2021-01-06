af5ccfc11b2f441df019c6ba6531e4fbf06944e6
Adrian <ad24.ra12@gmail.com>
2021-01-06 11:59:38 -0600

    Finished the functions for the program, the last step is to make the interface prettier

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter
 from tkinter import filedialog
+import os, sys, subprocess
 from view.file_creator import Creator
-import os, sys
 
 root_dir = os.getcwd()
 files_dir = root_dir+"/files"
@@ -13,7 +13,20 @@ def browse_files():
             title="Archivos...",
             filetypes = ( ("Archivos de texto","*.txt*"), ("Todos","*.*")) )
     print(chosen_file)
-
+def delete_file():
+    chosen_file = filedialog.askopenfilename(initialdir=files_dir,
+            title="Archivo a eliminar",
+            filetypes = ( ("Archivos de texto", "*.txt*") , ("Todos los archivos", "*.*")) )
+    if chosen_file != '':
+        #print("Deleted: %s" % chosen_file)
+        subprocess.run(["rm", chosen_file])
+def open_file():
+    chosen_file = filedialog.askopenfilename(initialdir=files_dir,
+         title="Archivo a eliminar",
+         filetypes = ( ("Archivos de texto", "*.txt*") , ("Todos los archivos", "*.*")) )
+    if chosen_file != '':
+        #print("Opening: %s" % chosen_file)
+        subprocess.run(["gedit",chosen_file] )
 
 #main window
 main_window = tkinter.Tk()
@@ -24,11 +37,11 @@ main_window.resizable(False, False)
 #Create button
 create_file = tkinter.Button(main_window, text="Crear archivo", command= create_file)
 #Delete button
-delete_button = tkinter.Button(main_window, text="Borrar archivo", command= lambda:print("delete") )
+delete_button = tkinter.Button(main_window, text="Borrar archivo", command= delete_file )
 #Show file explorer
 file_explorer = tkinter.Button(main_window, text="Mostrar archivos", command= browse_files ) 
 #Show file
-show_file = tkinter.Button(main_window, text="Abrir archivo", command = lambda:print("show 1 file") )
+show_file = tkinter.Button(main_window, text="Abrir archivo", command = open_file )
 
 create_file.pack()
 delete_button.pack()
